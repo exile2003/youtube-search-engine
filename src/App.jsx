01b91ebb00e66e426996f55e63fec54240759e96
exile2003 <31631692+exile2003@users.noreply.github.com>
2024-04-27 22:31:42 +0200

    Remove all unnecessary

--- src/App.jsx
+++ src/App.jsx
@@ -1,210 +1,89 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, memo, useCallback } from 'react'
+import {GridLoader, RingLoader} from 'react-spinners'
 import moment from 'moment'
-import debounce from 'lodash/debounce'
+import { debounce, throttle } from 'lodash'
+import ListItems from './components/ListItems'
+import Form from './components/Form'
+
 import './App.css'
 
-let youtubeDB = [];
-let tempDB =[];
+const MemoForm = memo(Form)
+const MemoListItems = memo(ListItems)
 
-let error, response = false;
+function renderByAnyKey() {
+    const [, forceRender] = useState()
 
-let resource;
-
+    useEffect(() => {
+        window.addEventListener("keydown", forceRender);
+        return () => window.removeEventListener("keydown", forceRender);
+        }, [])
+}
 
 function App() {
 
-  const [title, setTitle] = useState('');
-  const [channel, setChannel] = useState('');
-  const [dateFrom, setDateFrom] = useState('');
-  const [dateTo, setDateTo] = useState('');
+    renderByAnyKey()
+    
+  //const [title, setTitle] = useState('');
+  //const [channel, setChannel] = useState('');
   const [items, setItems] = useState([]);
-/*
-  const loadStatus = () => {
-    console.log("load status");
-    return new Promise(resolves => setTimeout(() => console.log("loaded"), 1000))
-  };
-*/
+  const [isLoading, setIsLoading] = useState(false);
 
+  //const [dateFrom, setDateFrom] = useState(() => '2017-01-01');
+ //const [dateTo, setDateTo] = useState(() => moment().format('YYYY-MM-DD'));
 
-  const getFile = (e) => {
-    
-    const threeSecondsToGnar = new Promise(resolves => {
+    // const updateDateFrom = useCallback(
+    //   setDateFrom
+    // , [])
 
-        const inputFile = e.target.files[0];
-        const reader = new FileReader();
-        reader.readAsText(inputFile);
-      
-        reader.onload = (event) => {
-          const fileContent = event.target.result;
-      
-          // Parsing the content of the input file and assign result to domTree variable
-          const domTree = new DOMParser().parseFromString(fileContent, 'text/html');
-      
-          // Pass the content of 'content-cell' and 'mdl-cell--6-col' classes to array allSelectors
-          const allSelectors = domTree.querySelectorAll('.content-cell.mdl-cell--6-col');
+    // const updateDateTo = useCallback(
+    //   setDateTo, [])
 
-          allSelectors.forEach(item => item.children[0] && youtubeDB.push({
-            title: item.children[0]?.textContent,
-            titleLink: item.children[0]?.href,
-            channel: item.children[2]?.textContent,
-            channelLink: item.children[2]?.href,
-            date: item.lastChild?.textContent
-          }));
-          
-          console.log("Yes!", youtubeDB)
-          resolves(true)
-        }
-        
+     const updateItems = useCallback(
+         (value) => setItems(value), [])
 
-      });
+    //    const updateItems = setItems;
 
-      resource = createResource(threeSecondsToGnar);
+     const updateIsLoading = useCallback(
+         (value) => setIsLoading(value), [])
 
-      //pending.then(r => response = r);  
+     //   const updateIsLoading = setIsLoading;
 
-      //if(response) return;
+    //const memoItems = useMemo(() => items, [items])
 
-      //throw pending;  
-  }
+    // const updateTitle = useCallback(
+    //   setTitle, [])
 
-  
-  const handleSubmit = (event) => {
-    event.preventDefault();
+    // const updateChannel = useCallback(
+    //   setChannel, [])
 
-    tempDB = [];
-
-    tempDB = youtubeDB
-                .filter(item => item.title?.toLowerCase().includes(title.trim().toLowerCase()))
-                .filter(item => item.channel?.toLowerCase().includes(channel.trim().toLowerCase()))
-                .filter(item => 
-                  item.date&&dateFrom 
-                    ? moment(item?.date, 'MMMM DD, YYYY, HH:mm:ss').unix() >= moment(dateFrom, 'YYYY-MM-DD').unix() 
-                    : true
-                )
-                .filter(item => 
-                  item.date&&dateTo 
-                    ? moment(item?.date, 'MMMM DD, YYYY, HH:mm:ss').unix() <= moment(dateTo, 'YYYY-MM-DD').unix() + 86400 
-                    : true
-                )
-
-    setItems(tempDB)
-
-    console.log('Название:', title);
-    console.log('Канал:', channel);
-    console.log('Дата:', dateFrom, dateTo);
-    console.log("youtubeDB", youtubeDB);
-    console.log("tempDB", tempDB);
-    console.log("items", items);
-    console.log("----------------------------------")
-    console.log("item.date", moment(items[1]?.date, 'MMMM DD, YYYY, HH:mm:ss').unix())
-    console.log("dateFrom", moment(dateFrom, 'YYYY-MM-DD').unix())
-
-  };
-
+    // const myProps = {
+    //   updateItems={updateItems} updateIsLoading={updateIsLoading} updateTitle={updateTitle} updateChannel={updateChannel} title={title} channel={channel}
+    // }
  
-  const getId = (arg) => {
-    const getid =  moment(arg, 'MMMM DD, YYYY, HH:mm:ss').unix() + String(Math.floor(Math.random()*1000));
-    //console.log("getid", getid);
-    return Number(getid)
-  }
- 
-  /*
-  const loadStatus = (function() {
-    let error, response;
-    const promise = new Promise(resolves =>
-    setTimeout(resolves, 3000)
-    )
-    .then(() => (response = "success"))
-    .catch(e => (error = e));
-    return function() {
-    if (error) throw error;
-    if (response) return response;
-    throw pending;
-    };
-   })()
-  */
+    /*
+    const myProps = {
+        updateItems, 
+        updateIsLoading
+    }
+    */
 
-   
-   function createResource(pending) {
-    
-    pending.then(r => (response = r)).catch(e => (error = e));
-      return {
-        read() {
-        if (error) throw error;
-        if (response) return response;
-        throw pending;
-        }
-      };
-   }
-  
-
-   /*
-   function createResource(pending) {
-    
-    pending.then(r => (response = r)).catch(e => (error = e));
-      
-        if (error) throw error;
-        if (response) return response;
-        throw pending;
-    
-   }
-*/
-
-  //const threeSecondsToGnar = new Promise(resolves => setTimeout( () => resolves({ gnar: "gnarly!" }), 3000 ))
-  
-  //const resource = createResource(threeSecondsToGnar);
-
- const result = resource.read()
-
+    useEffect(() => {
+      console.count("render App");
+    })
 /*
-  function Gnar() {
-    const result = resource.read();
-    return <h1>Gnar: {result.gnar}</h1>;
-  }
+     if (isLoading) return <>
+        <div className="spinner"><RingLoader /></div>
+       
+     </>
 */
-  
-  //const status = loadStatus();
-  //const statusResult = status.then(data => data)
-
   return ( 
     <>
-       <div className="container">
-        <div className="header">
-          <h2>Youtube videos</h2>
-          <label htmlFor="chooseFile">
-            <input onChange= {getFile} type="file" name="chooseFile" id="chooseFile"  />
-            <button >Обновить базу данных</button>
-          </label>
-       </div>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label>Название:&nbsp;&nbsp;</label>
-            <input type="text" onChange={debounce((e) => setTitle(e.target.value), 1000)} />
-          </div>
-          <div>
-            <label>Название канала:&nbsp;&nbsp;</label>
-            <input type="text" onChange={debounce((e) => setChannel(e.target.value), 1000)} />
-          </div>
-          <div>
-            <label>Дата:&nbsp;&nbsp;</label>
-            от&nbsp;
-            <input type="date" onChange={debounce((e) => setDateFrom(e.target.value), 100)} />
-            &nbsp;до&nbsp;
-            <input type="date" onChange={debounce((e) => setDateTo(e.target.value), 100)} />
-          </div>
-          <button type="submit">Искать</button>
-        </form>
-        <ul>
-          {items?.map(item =>  
-          <li key={getId(item.date)} >
-            <a href = {item.titleLink}>{item.title}</a> - 
-            <a href = {item.channelLink}>{item.channel}</a> - 
-            {moment(item.date, 'MMMM DD, YYYY, HH:mm:ss').format('MMMM-DD-YYYY')}
-          </li>)}
-        </ul>
+    {isLoading && <div className="spinner"><RingLoader /></div>}
+      <div style = {isLoading ? {display: 'none'} : {}}>
+        <MemoForm updateItems={setItems} updateIsLoading={setIsLoading} />
+        <MemoListItems items={items} />
       </div>
-    </> 
-          
+    </>       
   )
 }
 
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,61 +1,55 @@
-import { useState, useEffect, memo, useRef, useCallback } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import moment from 'moment'
-import { debounce, throttle } from 'lodash'
-//import '../App.css'
-
+//import { debounce, throttle } from 'lodash'
 
 let youtubeDB = [];
-let tempDB =[];
+let tempDB = [];
 
-
-function Form({
-      updateDateFrom,
-      updateDateTo,
+function Form({ 
       updateItems, 
       updateIsLoading, 
-      updateTitle, 
-      updateChannel,
-      dateFrom,
-      dateTo, 
-      title, 
-      channel}) {
+     }) {
 
-  // const [title, setTitle] = useState('');
-  // const [channel, setChannel] = useState('');
-  //const [dateFrom, setDateFrom] = useState(() => '2020-01-01');
-  //const [dateTo, setDateTo] = useState(() => moment().format('YYYY-MM-DD'));
-  //const [isLoading, setIsLoading] = useState(false);
+    const [title, setTitle] = useState('');
+    const [channel, setChannel] = useState('');
+    const [dateFrom, setDateFrom] = useState(() => '2017-01-01');
+    const [dateTo, setDateTo] = useState(() => moment().format('YYYY-MM-DD'));
 
-  const handleFileUpload = (event) => {
-    updateIsLoading(true);
-    const file = event.target.files[0];
+    const titlePrevious = useRef(null);
+    const channelPrevious = useRef(null);
+    const dateFromPrevious = useRef(null);
+    const dateToPrevious = useRef(null);
 
-    // Здесь можно выполнить логику загрузки файла
-    try {
-      // Предположим, что у вас есть функция для загрузки файла
-            if(file) getFile(file);
-    } catch (error) {
-      console.error('Ошибка загрузки файла:', error);
-    } finally {
-      //setIsLoading(false);
+    const handleFileUpload = (event) => {
+
+        const file = event.target.files[0];
+
+        try {
+          if(file) getFile(file);
+        } catch (error) {
+          console.error('Ошибка загрузки файла:', error);
+        }
     }
-  }
 
-  const getFile = (file) => {
+    const getFile = (file) => {
     
-        //const inputFile = e.target.files[0];
+        updateIsLoading(true);
+
+        youtubeDB = [];
+
         const reader = new FileReader();
         reader.readAsText(file);
       
         reader.onload = (event) => {
           const fileContent = event.target.result;
-      
+
           // Parsing the content of the input file and assign result to domTree variable
           const domTree = new DOMParser().parseFromString(fileContent, 'text/html');
       
           // Pass the content of 'content-cell' and 'mdl-cell--6-col' classes to array allSelectors
           const allSelectors = domTree.querySelectorAll('.content-cell.mdl-cell--6-col');
 
+          // Form the youtubeDB array with youtube videos data
           allSelectors.forEach(item => item.children[0] && youtubeDB.push({
             title: item.children[0]?.textContent,
             titleLink: item.children[0]?.href,
@@ -64,56 +58,62 @@ function Form({
             date: item.lastChild?.textContent
           }));
           
-          console.log("Yes!", youtubeDB);
+         // console.log("Yes!", youtubeDB);
           updateIsLoading(false)
         }    
-  }
+    }
 
-  const handleSubmit = (event) => {
+    const handleSubmit = (event) => {
     
-    event.preventDefault();
-    updateIsLoading(true);
+        event.preventDefault();
+ 
+        if(
+          title != titlePrevious.current |
+          channel != channelPrevious.current |
+          dateFrom != dateFromPrevious.current |
+          dateTo != dateToPrevious.current
+        ) {
+          updateIsLoading(true);
 
-    tempDB = [];
+          tempDB = filterYoutubeDB(youtubeDB, title, channel, dateFrom, dateTo);
+          updateItems(tempDB);
 
-    //title = txtTitle.current.value;
-    //channel = txtChannel.current.value;
+          titlePrevious.current = title;
+          channelPrevious.current = channel;
+          dateFromPrevious.current = dateFrom;
+          dateToPrevious.current = dateTo;
 
-    tempDB = youtubeDB
-                .filter(item => item.title?.toLowerCase().includes(title.trim().toLowerCase()))
-                .filter(item => item.channel?.toLowerCase().includes(channel.trim().toLowerCase()))
-                .filter(item => 
-                  item.date&&dateFrom 
-                    ? moment(item?.date, 'MMMM DD, YYYY, HH:mm:ss').unix() >= moment(dateFrom, 'YYYY-MM-DD').unix() 
-                    : true
-                )
-                .filter(item => 
-                  item.date&&dateTo 
-                    ? moment(item?.date, 'MMMM DD, YYYY, HH:mm:ss').unix() <= moment(dateTo, 'YYYY-MM-DD').unix() + 86400 
-                    : true
-                )
-              //  .filter(item => { setIsLoading(false); return true })
+          setTimeout(() => updateIsLoading(false), 0);
+        };
 
-              updateItems(tempDB)
-    setTimeout(() => updateIsLoading(false), 0)
+        console.log('Название:', title);
+        console.log('Канал:', channel);
+        console.log('Дата:', dateFrom, dateTo);
+        console.log("youtubeDB", youtubeDB);
+        console.log("tempDB", tempDB);
+        console.log("----------------------------------")
+        console.log("dateFrom", moment(dateFrom, 'YYYY-MM-DD').unix())
+        console.log('titlePrevious: ', titlePrevious);
+        console.log('channelPrevious: ', channelPrevious);
+    }
 
-    console.log('Название:', title);
-    console.log('Канал:', channel);
-    console.log('Дата:', dateFrom, dateTo);
-    console.log(typeof dateFrom);
-    console.log("youtubeDB", youtubeDB);
-    console.log("tempDB", tempDB);
-    //console.log("items", items);
-    console.log("----------------------------------")
-    //console.log("item.date", moment(items[1]?.date, 'MMMM DD, YYYY, HH:mm:ss').unix())
-    console.log("dateFrom", moment(dateFrom, 'YYYY-MM-DD').unix())
-    //console.log("isLoading", isLoading)
-    //console.log("txtTitle", txtTitle.current?.value)
+      const filterYoutubeDB = (youtubeDB, title, channel, dateFrom, dateTo) => {
+        const result = youtubeDB
+            .filter(item => item?.title?.toLowerCase().includes(title.trim().toLowerCase()))
+            .filter(item => item?.channel?.toLowerCase().includes(channel.trim().toLowerCase()))
+            .filter(item => 
+              item.date&&dateFrom 
+                ? moment(item?.date, 'MMMM DD, YYYY, HH:mm:ss').unix() >= moment(dateFrom, 'YYYY-MM-DD').unix() 
+                : true
+            )
+            .filter(item => 
+              item.date&&dateTo 
+                ? moment(item?.date, 'MMMM DD, YYYY, HH:mm:ss').unix() <= moment(dateTo, 'YYYY-MM-DD').unix() + 86400 
+                : true
+            );
+        return result
+      }
 
-  }
-
-
-    
     return (
       <div className="container">
         <div className="header">
@@ -127,18 +127,18 @@ function Form({
         <form onSubmit={handleSubmit}>
           <div>
             <label>Название:&nbsp;&nbsp;</label>
-            <input type="text" value={title} onChange={(e) => updateTitle(e.target.value)} />
+            <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
           </div>
           <div>
             <label>Название канала:&nbsp;&nbsp;</label>
-            <input type="text" value={channel} onChange={(e) => updateChannel(e.target.value)} />
+            <input type="text" value={channel} onChange={(e) => setChannel(e.target.value)} />
           </div>
           <div>
             <label>Дата:&nbsp;&nbsp;</label>
             от&nbsp;
-            <input type="date" value={dateFrom} onChange={(e) => updateDateFrom(e.target.value)} />
+            <input type="date" value={dateFrom} onChange={(e) => setDateFrom(e.target.value)} />
             &nbsp;до&nbsp;
-            <input type="date" value={dateTo} onChange={(e) => updateDateTo(e.target.value)} />
+            <input type="date" value={dateTo} onChange={(e) => setDateTo(e.target.value)} />
           </div>
           <button type="submit">Искать</button>
         </form>
